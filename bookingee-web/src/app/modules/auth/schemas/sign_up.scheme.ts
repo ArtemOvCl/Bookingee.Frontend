e406aa6b0fa63121c624a6107f7f3cf115442c1b
ArtemOvCl <120483835+ArtemOvCl@users.noreply.github.com>
2025-09-25 10:47:49 +0300

    Initialize Next.js project with auth module
    Set up a new Next.js app with TypeScript, ESLint, Tailwind CSS, and authentication scaffolding. Added initial configuration files, public assets, and basic sign-up form structure.

---
+++ bookingee-web/src/app/modules/auth/schemas/sign_up.scheme.ts
@@ -0,0 +1,15 @@
+import { z } from "zod"
+
+export const signupSchema = z
+  .object({
+    name: z.string().min(2, { message: "Ім’я має містити щонайменше 2 символи" }),
+    email: z.string().email({ message: "Некоректний email" }),
+    password: z.string().min(6, { message: "Пароль має містити мінімум 6 символів" }),
+    confirmPassword: z.string(),
+  })
+  .refine((data) => data.password === data.confirmPassword, {
+    message: "Паролі не збігаються",
+    path: ["confirmPassword"],
+  })
+
+export type RegisterInput = z.infer<typeof signupSchema>
